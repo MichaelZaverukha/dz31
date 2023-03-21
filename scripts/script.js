2b99e912fd556e115de4fab669b6c8901b952a41
Michael Zaverukha <zorkiz28@gmail.com>
2023-03-21 13:59:40 +0200

    dz31

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+$('.main-section__title-list').slick({
+   dots:true,
+   infinite:true,
+});
+
+$('.main-section__arrivals-list').slick({
+   infinite: true,
+   slidesToShow: 5,
+   slidesToScroll: 1,
+   centerMode:true,
+   variableWidth: true,
+   autoplay: true,
+  autoplaySpeed: 1000,
+ });
+
+
+ $('.main-section__partners-list').slick({
+   infinite: true,
+   slidesToShow: 9,
+   slidesToScroll: 1,
+   centerMode:true,
+   variableWidth: true,
+ });
